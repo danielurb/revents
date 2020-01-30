9d445b54c55b8048fe9dcfac2735825055e45445
Daniel Urbanowicz <urbanowicz.daniel@gmail.com>
2020-01-30 10:22:13 +0100

    End of section 8

--- src/index.js
+++ src/index.js
@@ -6,10 +6,9 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './app/store/configureStore';
+import ScrollToTop from './app/common/util/ScrollToTop';
 
 const store = configureStore();
-
-console.log(store.getState());
 
 const rootEl = document.getElementById('root');
 
@@ -17,6 +16,7 @@ let render = () => {
   ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
+        <ScrollToTop />
         <App />
       </BrowserRouter>
     </Provider>,
